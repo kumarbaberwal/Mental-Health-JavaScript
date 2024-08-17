9446b16290e2423b20d31c574862955006596634
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-08-17 23:04:11 +0530

    print songs on the console

--- adapters/controllers/songcontroller.js
+++ adapters/controllers/songcontroller.js
@@ -6,6 +6,8 @@ class SongController {
             const getSongs = new GetSongs();
             const songs = await getSongs.execute();
             res.json(songs);
+            // console.log(songs);
+            // console.log(res.json(songs));
         } catch (error) {
             res.status(500).json({ error: error.message });
         }
